789bafc26b3e724eebc3fbf6e8d6bec60e87d1dc
Tenzin Choezom <khortsa2021@gmail.com>
2022-05-10 13:51:53 +0200

    first commit router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
 import './App.css';
-import Login from './Components/Login';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Components/Home'
+import Profile from './Components/Profile'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 
 function App() {
   return (
     <div className="App">
-    <Login/>
-    </div>
+      <nav>
+        <ul>
+          <li><Link to='/home'>Home</Link></li>
+          <li><Link to='/profile'>Profile</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+          <li><Link to='/signup'>Signup</Link></li>
+        </ul>
+      </nav>
+        <Routes>
+          <Route exact path='/home' element={<Home/>}></Route>
+          <Route path='profile' element={<Profile/>}></Route>
+          <Route path='login' element={<Login/>}></Route>
+          <Route path='signup' element={<Signup/>}></Route>
+        </Routes>
+
+    </div> 
   );
 }
 
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="container">
+      <div className="row  text-center">
+        <div className="col-3">Column</div>
+        <div className="col-6">Column</div>
+        <div className="col-3">Column</div>
+      </div>
+    </div>
+  );
+}
+export default Profile;
